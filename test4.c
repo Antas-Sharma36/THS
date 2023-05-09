81cdca859b9189bedbf06ec03bff43c900e3f42b
Antas-Sharma36 <sharmaantas20@gmail.com>
2023-05-09 19:16:15 +0530

    single command is being replaced properly

--- test4.c
+++ test4.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <readline/history.h>
-#include<readline/readline.h>
+#include <readline/readline.h>
+#include <stdlib.h>
 
 int main() {
-  using_history();
-  read_history("history_file");
+  char *line;
+  HIST_ENTRY *entry;
 
 
+// specify the history file
+  char* history_file = "history_file";
+
+// read the history file 
+  read_history(history_file);
 
   register HIST_ENTRY **the_list;
   register int i;
 
+          the_list = history_list();
+          if (the_list)
+            for (i = 0; the_list[i]; i++)
+              printf ("%d: %s\n", i + history_base, the_list[i]->line);
+
+
+//  replace the second history entry
+  replace_history_entry(1, "new command", NULL);
+
+// print the updated history file 
+  printf("the updated history is:\n");
   the_list = history_list();
-    if (the_list)
-        for (i = 0; the_list[i]; i++)
-            printf ("%d: %s\n", i + history_base, the_list[i]->line);
-  HIST_ENTRY *entry = history_get(history_length);
-  printf("Last command: %s\n", entry->line);
+  if (the_list)
+    for (i = 0; the_list[i]; i++)
+      printf ("%d: %s\n", i + history_base, the_list[i]->line);
 
-//   char *new_command = "ls -al";
-//   replace_history(history_length - 1, new_command, NULL);
-
-//   write_history("history.txt");
   return 0;
 }
